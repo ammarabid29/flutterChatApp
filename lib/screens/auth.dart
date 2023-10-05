ec67ac4841e233f4d7b1745a42d1bf2417be5ca7
ammarabid29 <aammarawan786@gmail.com>
2023-10-05 15:04:58 +0500

    firebase sdk and user sign up

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -13,11 +16,29 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredPassword = '';
   final _formKey = GlobalKey<FormState>();
 
-  void _submit() {
+  void _submit() async {
     final isValid = _formKey.currentState!.validate();
 
-    if (isValid) {
-      _formKey.currentState!.save();
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+    if (_isLogin) {
+      ///
+    } else {
+      try {
+        final userCredientials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+        print(userCredientials);
+      } on FirebaseAuthException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          ///
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(error.message ?? "Authentication Failed")));
+      }
     }
   }
 
